40164cc3b7b34bb553e67d0636e79e8946b03612
iramos <isma@metastartup.com>
2018-12-17 15:03:37 +0100

    New template with under construction message

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,44 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-let header = require('gulp-header');
-let cleanCSS = require('gulp-clean-css');
-let rename = require("gulp-rename");
-let uglify = require('gulp-uglify');
-let autoprefixer = require('gulp-autoprefixer');
-let pkg = require('./package.json');
-let browserSync = require('browser-sync').create();
+/**
+ * Created by pharaoh on 18/12/2017.
+ */
 
-// Set the banner content
-const banner = ['/*!\n',
-  ' * Jesus Peñalver - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2018-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * https://github.com/Ismaestro/jesuspenalver/blob/master/LICENSE\n',
-  ' */\n',
-  '\n'
-].join('');
+'use strict';
+const gulp = require('gulp');
+const sass = require("gulp-ruby-sass");
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const livereload = require("gulp-livereload");
 
-// Compile SCSS
-gulp.task('css:compile', function () {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./css'))
+
+
+gulp.task('style',function () {
+    sass('assets/scss/**/*.scss',{ style: 'expanded' })
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(gulp.dest('assets/css/'))
+        .pipe(livereload());
 });
 
-// Minify CSS
-gulp.task('css:minify', gulp.series('css:compile'), function () {
-  return gulp.src([
-    './css/*.css',
-    '!./css/*.min.css'
-  ])
-    .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+//minify all js to one js file
+gulp.task('minscripts', function() {
+    return gulp.src(['assets/js/jquery-migrate.js', 'assets/js/jquery.waypoints.min.js','assets/js/popper.js', 'assets/js/bootstrap.min.js','assets/js/wow.min.js','assets/js/bootstrap-progressbar.min.js', 'assets/js/jquery.counterup.min.js','assets/js/slick.min.js', 'assets/js/jquery.magnific-popup.min.js' ,'assets/js/jquery.filterizr.min.js'])
+        .pipe(concat('plugins.js'))
+        .pipe(gulp.dest('assets/'));
 });
 
-// CSS
-gulp.task('css', gulp.series('css:compile', 'css:minify'));
+//minify all css to one css file
 
-// Minify JavaScript
-gulp.task('js:minify', function () {
-  return gulp.src([
-    './js/*.js',
-    '!./js/*.min.js'
-  ])
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browserSync.stream());
+gulp.task('mincss',function () {
+    return gulp.src(['assets/css/bootstrap.min.css','assets/css/animate.min.css','assets/css/et-line.css','assets/css/font-awesome.min.css','assets/css/magnific-popup.css','assets/css/slick.css','assets/css/ionicons.min.css'])
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('assets/'))
 });
 
-// JS
-gulp.task('js', gulp.series('js:minify'));
 
-// Default task
-gulp.task('default', gulp.series('css', 'js'));
-
-// Configure the browserSync task
-gulp.task('browserSync', function () {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
+gulp.task('watch',function () {
+    livereload.listen();
+    gulp.watch('assets/scss/**/*.scss',['style'])
 });
 
-// Dev task
-gulp.task('dev', gulp.series('css', 'js', 'browserSync'), function () {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
-  gulp.watch('./*.html', browserSync.reload);
-});
+gulp.task('default',['style','watch']);
